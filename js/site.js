f6fc9c7ea576838d2d91acdbbc802a77277c3e63
Mareks "NotS1ick" Lovniks <69733624+NotS1ick@users.noreply.github.com>
2024-10-30 22:50:03 +0200

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,27 @@
+﻿function updateLogo() {
+    const logoImage = document.getElementById("logoImage");
+    if (window.innerWidth <= 425) {
+        logoImage.src = "/logo100x24.png";
+    } else {
+        logoImage.src = "/logo154x48.png";
+    }
+}
+
+updateLogo();
+
+window.addEventListener("resize", updateLogo);
+
+function updateCollapse() {
+    const ContactUs = document.getElementById("ContactUs");
+    const TermsOfService = document.getElementById("TermsOfService");
+    const navBarNav = ContactUs.parentElement;
+    
+    if (window.innerWidth <= 576) {
+        navBarNav.insertBefore(TermsOfService, ContactUs);
+    } else {
+        navBarNav.insertBefore(ContactUs, TermsOfService);
+    }
+}
+
+window.addEventListener("load", updateCollapse);
+window.addEventListener("resize", updateCollapse);
